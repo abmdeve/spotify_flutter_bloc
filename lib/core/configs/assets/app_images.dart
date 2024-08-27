5f43b093bae0da7289adc8368b62893f18eb257c
abmdeve <tundederome@gmail.com>
2024-08-27 13:04:57 +0200

    Update the code
    Get Started Screen is implemented

---
+++ lib/core/configs/assets/app_images.dart
@@ -0,0 +1,9 @@
+
+
+
+
+class AppImages {
+  static const String basePath = 'assets/images/';
+  static const String format = '.png';
+  static const String introBG = '${basePath}intro_bg$format';
+}
--- lib/presentation/splash/pages/splash_screen.dart
+++ lib/presentation/splash/pages/splash_screen.dart
@@ -1,18 +1,40 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:spotify_flutter_bloc/core/configs/assets/app_vectors.dart';
+import 'package:spotify_flutter_bloc/presentation/intro/pages/get_started_screen.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    redirect();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
         child: SvgPicture.asset(
-            AppVectors.logo,
+          AppVectors.logo,
         ),
       ),
     );
+  }
+
+  Future<void> redirect() async {
+    await Future.delayed(const Duration(seconds: 2));
+    Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+            builder: (BuildContext context) => const GetStartedScreen()));
   }
 }

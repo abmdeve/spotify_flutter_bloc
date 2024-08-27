9b18911c9fc88a7d221aa9951b3a73dcbfe4bea2
abmdeve <tundederome@gmail.com>
2024-08-27 11:51:28 +0200

    Update the code
    Assets & Fonts Folders / Defining in Pubspec

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:spotify_flutter_bloc/core/configs/theme/app_theme.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,11 +11,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
+      ///title: 'Flutter Demo',
+      theme: AppTheme.lightTheme,
       home: Container(),
     );
   }
